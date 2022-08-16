7448b949665b5428fff481c4d05702ad2bace769
Mikhail Bootko <m14ai1@yandex.ru>
2022-08-16 13:23:08 +0300

    fix modal localStorage

--- build/js/modules/formdata.js
+++ build/js/modules/formdata.js
@@ -1,7 +1,7 @@
 const getFormData = (element) => {
-  const name = element.querySelector('#name').value;
-  const phone = element.querySelector('#phone').value;
-  const quest = element.querySelector('#question').value;
+  const name = element.querySelector('input[name=name]').value;
+  const phone = element.querySelector('input[name=phone]').value;
+  const quest = element.querySelector('textarea[name=question]').value;
   localStorage.setItem('name', name);
   localStorage.setItem('phone', phone);
   localStorage.setItem('quest', quest);
